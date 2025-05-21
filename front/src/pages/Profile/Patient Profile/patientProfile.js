d2a288f4e45fdb27f040bc62922cc5475d495644
Isaac Njenga <ayzzoh20@gmail.com>
2025-05-21 13:37:41 +0300

    nth commit

--- front/src/pages/Profile/Patient Profile/patientProfile.js
+++ front/src/pages/Profile/Patient Profile/patientProfile.js
@@ -6,15 +6,54 @@ import MedicalInfo from "./medicalInfo.js";
 import AIInsight from "./AIInsight.js";
 import Reports from "./reports.js";
 import Logs from "./logs.js";
+import {
+  BarChartOutlined,
+  FileDoneOutlined,
+  InfoCircleOutlined,
+  LockOutlined,
+  RobotOutlined,
+  UserOutlined,
+} from "@ant-design/icons";
 
+const iconStyle = { fontSize: "1.5rem" };
 function PatientProfile({ user }) {
   const tabItems = [
-    { key: 1, name: "Account & Privacy", childPage: <UserAccount /> },
-    { key: 2, name: "Personal Information", childPage: <PersonalInfo /> },
-    { key: 3, name: "Medical Information", childPage: <MedicalInfo /> },
-    { key: 4, name: "AI Recommendations", childPage: <AIInsight /> },
-    { key: 5, name: "Document & Reports", childPage: <Reports /> },
-    { key: 6, name: "Health Diary/Logs", childPage: <Logs /> },
+    {
+      key: 1,
+      name: "Account & Privacy",
+      childPage: <UserAccount />,
+      icon: <LockOutlined style={iconStyle} />,
+    },
+    {
+      key: 2,
+      name: "Personal Information",
+      childPage: <PersonalInfo />,
+      icon: <UserOutlined style={iconStyle} />,
+    },
+    {
+      key: 3,
+      name: "Medical Information",
+      childPage: <MedicalInfo />,
+      icon: <InfoCircleOutlined style={iconStyle} />,
+    },
+    {
+      key: 4,
+      name: "AI Recommendations",
+      childPage: <AIInsight />,
+      icon: <RobotOutlined style={iconStyle} />,
+    },
+    {
+      key: 5,
+      name: "Document & Reports",
+      childPage: <Reports />,
+      icon: <BarChartOutlined style={iconStyle} />,
+    },
+    {
+      key: 6,
+      name: "Health Diary/Logs",
+      childPage: <Logs />,
+      icon: <FileDoneOutlined style={iconStyle} />,
+    },
   ];
   return (
     <Tabs
@@ -22,7 +61,9 @@ function PatientProfile({ user }) {
       items={tabItems.map((item) => ({
         label: (
           <p>
-            <strong>{item.name}</strong>
+            <strong>
+              {item.icon} {item.name}
+            </strong>
           </p>
         ),
         key: String(item.key),
